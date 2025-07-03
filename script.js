36403d858eaf939d45cb98557aa77c1bee70e16b
haris-afr <haris.afridi1312@gmail.com>
2025-07-03 17:44:26 +0500

    create structure for calculator

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const middleRow = document.querySelector(".calculator-mid-row");
+const bottomRow = document.querySelector(".calculator-bottom-row");
+
+
 function cprint(val) {console.log(val);}
 function add(a, b) {return a + b;}
 function subtract(a, b) {return a - b;}
@@ -26,5 +30,30 @@ function operate (a, b, operator){
         default:
             print("Error in operation!");
             break;
+    }
+}
+
+function createMiddleRowButton(){
+    const btn = document.createElement("button");
+    btn.classList.add("middle-row-button");
+    middleRow.appendChild(btn);
+}
+
+function createBottomRowButton(isGreen=false){
+    const btn = document.createElement("button");
+    btn.classList.add("bottom-row-button");
+    if (isGreen){
+        btn.classList.add("green-btn");
+    }
+    bottomRow.appendChild(btn);
+}
+
+for (i = 0; i < 18; i++){createMiddleRowButton();}
+for(i = 0; i < 20; i++){
+    if (i != 3 && i != 4){
+        createBottomRowButton();
+    }
+    else{
+        createBottomRowButton(true);
     }
 }
