c9113b74e4507f438a25461a3b8e9a4fc4954e70
haris-afr <haris.afridi1312@gmail.com>
2025-07-03 12:14:19 +0500

    add basic functions

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hello world!");
+function add(a, b) {return a + b;}
+function subtract(a, b) {return a - b;}
+function multiply(a, b) {return a * b;}
+function divide(a, b) {return a / b;}
+function addArray(arr){
+    return arr.reduce((sum, index) => sum + index);
+}
+function multiplyArray(arr){
+    return arr.reduce((product, index) => product * index);
+}
+function cprint(val) {console.log(val);}
+
+cprint(divide(40, -20));
